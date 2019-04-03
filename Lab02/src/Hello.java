fa35f49bd7f867127a8e17ca74fa3b7d6a777b2e
lucero <lucero@192.168.0.105>
2019-04-02 18:49:34 -0700

    lab2-lucero added new line with my info

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("301054598 - This line was added by Austin Rasmussen.");
 		System.out.println("219670114 - This line was added by Uthej Mopathi.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("214120479 - This line was added by Lucero Rodriguez.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
