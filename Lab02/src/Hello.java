bf40d90c43c1af7027f1e81b81be3947712b867b
Uthej Mopathi <mopathiuthej4@gmail.com>
2019-04-02 18:26:33 -0700

    Update Hello.java

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301054598 - This line was added by Austin Rasmussen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219670114 - This line was added by Uthej Mopathi.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
